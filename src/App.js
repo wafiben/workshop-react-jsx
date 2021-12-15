4f86f4e7c979ac0226b557ad662f5a7d33cb241e
wafiben <wafibenjeddou@gmail.com>
2021-12-15 16:56:32 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from "./Home.js"
+import Form from "./Form.js"
+import Footer from "./Components/Footer.js"
 
-function App() {
+const App=()=>{
+  const name='ahmed'
+  const age =20
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <> 
+  <Form/>
+  <Home/> 
+  <Footer/>
 
-export default App;
+    </>
+
+
+  )
+}
+export default App
---
+++ src/Components/Footer.js
@@ -0,0 +1,9 @@
+import "./footer.css"
+const Footer=()=>{
+    return(
+    <div className="parent">
+        <h1>Footer Component</h1>
+        </div>
+    )
+}
+export default Footer
